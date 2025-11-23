1451f982db48cdfb08b9be6cb1eabde438bdb30e
YammD123 <siamqueen52@gmail.com>
2025-11-23 16:53:24 +0700

    test CD 4

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello saya belajar CI/CD'))
-app.get('/say', (c) => c.text('HALLO BOSS APA KABAR'))
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
 serve({
   fetch: app.fetch,
-  port: Number(process.env.PORT) || 3000,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
 })
-
-console.log("Server is running...")
